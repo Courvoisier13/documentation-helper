c9f74a340c2449b04a449e28477f9e314421e15a
Khalil <Courvoisier13@users.noreply.github.com>
2024-11-25 16:12:16 +0000

    Update core.py

--- backend/core.py
+++ backend/core.py
@@ -18,9 +18,15 @@ def run_llm(query:str):
 
     qa = create_retrieval_chain(retriever=docsearch.as_retriever(), combine_docs_chain=stuff_documents_chain)
     result = qa.invoke(input = {"input": query})
-    return result
+
+    new_result = {
+        "query": result["input"],
+        "result": result["answer"],
+        "source_document": result["context"]
+    }
+    return new_result
 
 if __name__=="__main__":
     query = "What is a langchain chain"
     result = run_llm(query)
-    print(result)
+    print(result["result"])
